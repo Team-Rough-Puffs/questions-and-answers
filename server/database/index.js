82f2499ad7fe6dbf38489179954c2b76bbc15183
Tyna Nguyen <ngtyna@gmail.com>
2021-01-29 17:20:22 -0600

    Install more package dependencies and clean up routes

--- server/database/index.js
+++ server/database/index.js
@@ -2,40 +2,47 @@
 /****************** POSTGRES CONNECTION ********************/
 /***********************************************************/
 
-// pg-promise
-// var pgp = require('pg-promise')(/*options*/);
-// var pgConfig = 'postgres://localhost:5432/questions_answers';
-// var db = pgp(pgConfig);
+var pgp = require('pg-promise')(/*options*/);
+var pgConfig = 'postgres://localhost:5432/questions_answers';
+var db = pgp(pgConfig);
 
-// Difference between Client vs. Pool?
-// Client ==> one static connection
-// Pool ==> manages dynamic list of Client objects
-// (use if you expect to have multiple concurrent requests)
+// const { Client, Pool } = require('pg');
+// const pgConfig = require('./config.js');
 
-const { Client, Pool } = require('pg');
-const pgConfig = require('./config.js');
-
-const client = new Client(pgConfig);
-client.connect();
+// const client = new Client(pgConfig);
+// client.connect();
 
 /***********************************************************/
 /************************ QUERIES **************************/
 /***********************************************************/
 
 const getQuestionsForProduct = (productId, callback) => {
-  client.query(`SELECT * FROM questions WHERE product_id = ${productId}`)
+  db.query('SELECT * FROM questions WHERE product_id = $1', productId)
     .then((data) => {
-      callback(data);
+      callback(null, data);
     })
     .catch((error) => {
       callback(error);
     })
 };
 
-const getAnswersForQuestion = (id, callback) => {
-  client.query(`SELECT * FROM answers WHERE question_id = ${id}`)
+const getAnswersForQuestion = (questionId, callback) => {
+  db.query('SELECT * FROM answers WHERE question_id = $1', questionId)
     .then((data) => {
-      callback(data);
+      callback(null, data);
+    })
+    .catch((error) => {
+      callback(error);
+    })
+};
+
+const addQuestion = (productId, body, asker_name, asker_email, callback) => {
+  // Define a new date object when a new question is posted
+  let date = new Date();
+  let date_written = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+  db.query(`INSERT INTO questions(product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES (1, 'hello', '${date_written}', 'anon', 'anon@email.com', ${0}, ${0})`)
+    .then((data) => {
+      callback(null, data);
     })
     .catch((error) => {
       callback(error);
@@ -44,8 +51,8 @@ const getAnswersForQuestion = (id, callback) => {
 
 module.exports = {
   getQuestionsForProduct,
-  getAnswersForQuestion
-  // addQuestion,
+  getAnswersForQuestion,
+  addQuestion
   // addAnswer,
   // markQuestionHelpful,
   // reportQuestion,
